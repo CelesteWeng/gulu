89c6ffb37b6bf2a637530aecf32859ca2c3642e2
celesteweng <673319951@qq.com>
2019-04-16 21:07:04 +0800

    添加 loading 状态

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,9 @@
 <template>
     <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
         <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
             <slot/>
-            <!--<slot></slot>-->
         </div>
     </button>
 </template>
@@ -23,6 +23,11 @@
     }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+
+    }
     .g-button {
         display: inline-flex;
         justify-content: center;
@@ -35,14 +40,17 @@
         border: 1px solid var(--border-color);
         background: var(--button-bg);
         &:hover { border-color: var(--border-color-hover); }
-        &:active {background-color: var(--button-active-bg);}
-        &:focus {outline: none;}
-        > .icon {order: 1;margin-right: .1em;}
-        > .content {order: 2;}
+        &:active { background-color: var(--button-active-bg); }
+        &:focus { outline: none; }
+        > .icon { order: 1;margin-right: .1em; }
+        > .content { order: 2; }
 
         &.icon-right {
-            > .icon {order: 2;margin-left: .1em;}
-            > .content {order: 1;}
+            > .icon { order: 2;margin-left: .1em; }
+            > .content { order: 1; }
         }
+    }
+    .loading {
+        animation: 2s spin infinite linear;
     }
 </style>
