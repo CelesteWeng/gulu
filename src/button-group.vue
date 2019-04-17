e16f05077310dd59c59d78b9711841c735711c7e
celesteweng <673319951@qq.com>
2019-04-17 15:52:19 +0800

    检测 g-button-group 的子元素是不是都为 button

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,7 +4,16 @@
     </div>
 </template>
 <script>
-    export default {}
+    export default {
+        mounted() {
+            for (let node of this.$el.children) {
+                let name = node.nodeName.toLowerCase()
+                if (name !== "button") {
+                    console.warn(`g-button-group 的自元素只能为 g-button, 而你插入的是 ${name}`)
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
     .g-button-group {
