b4e18bfe176ce9d6ac3f93a14fc9ec127d2b0e93
celesteweng <673319951@qq.com>
2019-04-17 16:00:18 +0800

    第一个元素不需要 margin left -1px，并删除 hover red

--- src/button-group.vue
+++ src/button-group.vue
@@ -17,11 +17,10 @@
 </script>
 <style lang="scss">
     .g-button-group {
-        display: inline-flex;
-        vertical-align: middle;
+        display: inline-flex; vertical-align: middle;
         > .g-button {
             border-radius: 0;
-            margin-left: -1px;
+            &:not(:first-child) { margin-left: -1px; }
             &:first-child {
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
@@ -30,10 +29,7 @@
                 border-top-right-radius: var(--border-radius);
                 border-bottom-right-radius: var(--border-radius);
             }
-            &:hover {
-                position: relative;
-                z-index: 1;
-            }
+            &:hover { position: relative; z-index: 1; }
         }
     }
 </style>
