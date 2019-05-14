59bb6b7d6fe52fa7e59f95f0bd4e4f462bdefbf1
celesteweng <673319951@qq.com>
2019-05-14 21:51:53 +0800

    完成 row 和 col 的雏形

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
-import Vue from "vue"
-import Button from "./button"
-import Icon from "./icon"
-import ButtonGroup from "./button-group"
-import Input from "./input"
+import Vue from 'vue'
+import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
+import Input from './input'
+import Row from './row'
+import Col from './col'
 
-Vue.component("g-button", Button)
-Vue.component("g-icon", Icon)
-Vue.component("g-button-group", ButtonGroup)
-Vue.component("g-input", Input)
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     loading1: false,
     loading2: true,
---
+++ src/col.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'GuluCol',
+    props: {
+      span: {
+        type: [Number, String]
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .col {
+    height: 100px;
+    background: grey;
+    width: 50%;
+    border: 1px solid red;
+  }
+  $class-prefix: col-;
+
+  @for $n from 1 through 24 {
+    .#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+      background-color: darken(cornflowerblue, 0% + ($n / 2));
+    }
+  }
+  /*.col[data-span="2"] {*/
+  /*  width: 8.333333%;*/
+  /*}*/
+  /*.col[data-span="22"] {*/
+  /*  width: 91.666667%;*/
+  /*}*/
+</style>
